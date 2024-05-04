46ea93c366dd1c4b047987e783e97185b8a3e22c
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-05-04 18:12:04 +0300

    order implementations done

--- src/main/java/com/enoca/challenge/atakanaksoy/business/concretes/OrderManager.java
+++ src/main/java/com/enoca/challenge/atakanaksoy/business/concretes/OrderManager.java
@@ -7,36 +7,59 @@ import com.enoca.challenge.atakanaksoy.business.dtos.responses.order.CreatedOrde
 import com.enoca.challenge.atakanaksoy.business.dtos.responses.order.GetAllOrdersResponse;
 import com.enoca.challenge.atakanaksoy.business.dtos.responses.order.GetOrderByIdResponse;
 import com.enoca.challenge.atakanaksoy.business.dtos.responses.order.UpdatedOrderResponse;
+import com.enoca.challenge.atakanaksoy.business.rules.OrderBusinessRules;
+import com.enoca.challenge.atakanaksoy.core.utilities.mapping.ModelMapperService;
+import com.enoca.challenge.atakanaksoy.dataAccess.abstracts.OrderRepository;
+import com.enoca.challenge.atakanaksoy.entities.concretes.Order;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class OrderManager implements OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderBusinessRules orderBusinessRules;
+    private final ModelMapperService modelMapperService;
     @Override
     public CreatedOrderResponse add(CreateOrderRequest createOrderRequest) { //placeOrder
-        return null;
+        Order order = modelMapperService.forRequest().map(createOrderRequest, Order.class);
+        order.setActive(true);
+        order.setCreateDate(LocalDateTime.now());
+        Order savedOrder = orderRepository.save(order);
+        return modelMapperService.forResponse().map(savedOrder, CreatedOrderResponse.class);
     }
 
     @Override
     public UpdatedOrderResponse update(UpdateOrderRequest updateOrderRequest, int id) {
-        return null;
+        Order order = orderBusinessRules.orderMustExists(id);
+        modelMapperService.forUpdate().map(updateOrderRequest, order);
+        order.setUpdateDate(LocalDateTime.now());
+        Order savedOrder = orderRepository.save(order);
+        return modelMapperService.forResponse().map(savedOrder, UpdatedOrderResponse.class);
     }
 
     @Override
     public void delete(int id) {
-
+        Order order = orderBusinessRules.orderMustExists(id);
+        order.setActive(false);
+        order.setDeleteDate(LocalDateTime.now());
+        orderRepository.save(order);
     }
 
     @Override
     public GetOrderByIdResponse getById(int id) {
-        return null;
+        Order order = orderBusinessRules.orderMustExists(id);
+        return modelMapperService.forResponse().map(order, GetOrderByIdResponse.class);
     }
 
     @Override
     public List<GetAllOrdersResponse> getAll() {
-        return null;
+        List<Order> orders = orderRepository.findAllByActiveTrue();
+        return orders.stream().map(
+                order -> modelMapperService.forResponse().map(order, GetAllOrdersResponse.class)
+        ).toList();
     }
 }
--- src/main/java/com/enoca/challenge/atakanaksoy/dataAccess/abstracts/OrderRepository.java
+++ src/main/java/com/enoca/challenge/atakanaksoy/dataAccess/abstracts/OrderRepository.java
@@ -3,5 +3,8 @@ package com.enoca.challenge.atakanaksoy.dataAccess.abstracts;
 import com.enoca.challenge.atakanaksoy.entities.concretes.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findAllByActiveTrue();
 }
